9a5e0464d9d51ae7a732ccb6126327d2a5d6c532
橙子酱 <cfd0123456789@qq.com>
2020-08-14 09:32:39 +0800

    CVE-2017-12776

--- takeupdate.php
+++ takeupdate.php
@@ -11,13 +11,13 @@ loggedinorreturn();
 if (get_user_class() < $staffmem_class)
        permissiondenied();
 if ($_POST['setdealt']){
-$res = sql_query ("SELECT id FROM reports WHERE dealtwith=0 AND id IN (" . implode(", ", $_POST[delreport]) . ")");
+$res = sql_query ("SELECT id FROM reports WHERE dealtwith=0 AND id IN (" . sqlesc(implode(", ", $_POST[delreport])) . ")");
 while ($arr = mysql_fetch_assoc($res))
 	sql_query ("UPDATE reports SET dealtwith=1, dealtby = $CURUSER[id] WHERE id = $arr[id]") or sqlerr();
 	$Cache->delete_value('staff_new_report_count');
 }
 elseif ($_POST['delete']){
-$res = sql_query ("SELECT id FROM reports WHERE id IN (" . implode(", ", $_POST[delreport]) . ")");
+$res = sql_query ("SELECT id FROM reports WHERE id IN (" . sqlescimplode(", ", $_POST[delreport])) . ")");
 while ($arr = mysql_fetch_assoc($res))
 	sql_query ("DELETE from reports WHERE id = $arr[id]") or sqlerr();
 	$Cache->delete_value('staff_new_report_count');
